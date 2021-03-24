d4a3fc7c249ed7e65844d7d3d49699d8970da334
Shenila Letishya Ramona <71870272+ShenilaLetishyaRamona@users.noreply.github.com>
2021-03-24 21:37:57 +0700

    Tugas 3/2

---
+++ Laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index()
+    {
+        return 'Ini Profil Controller';
+    }
+
+    public function create()
+    {
+         return 'Method Ini Nantinya Untuk Digunakan Untuk Menampilkan Form Data User'; 
+    }
+
+
+    public function store(Request $request)
+    {
+         return 'Method Ini Nantinya Untuk Digunakan Untuk Menciptakan Data User Yang Baru'; 
+    }
+
+    public function show($id)
+    {
+        return 'Method Ini Mestinya Akan Digunakan Untuk Mengambil Satu Data User Dengan Id '.$id;
+    }
+    public function edit($id)
+    {
+        return 'Method Ini Nantinya Akan Digunakan Untuk Menampilkan Form Untuk Mengubah Data Edit Dengan Id '.$id;
+    }
+}
